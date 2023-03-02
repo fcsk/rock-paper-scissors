13f7d374a543df3ee21fb89d31802f471b03b194
fcsk <kfocusek@gmail.com>
2023-03-02 21:41:56 +0100

    First conception mechanics for this game. The result will be display in console.

--- js/main.js
+++ js/main.js
@@ -1,43 +1,41 @@
-(function(){
+const selectedDiV = document.querySelector(".player-weapon");
 
+selectedDiV?.addEventListener("click", function (event) {
+  let value = "";
+  let string = event.target?.className;
+  if (string == "icon-hand-scissors-o" || string == "scissors") {
+    value = "scissors";
+  } else if (string == "icon-hand-paper-o" || string == "paper") {
+    value = "paper";
+  } else if (string == "icon-hand-grab-o" || string == "rock") {
+    value = "rock";
+  }
+  const playerValue = value;
+  const computerValue = GenerateComputerChoose();
+  console.log("Grasz wybrał", value);
+  console.log("Komputer wybrał", computerValue);
+  ComparePlayerToComputer(playerValue, computerValue);
+});
 
-function GenerateComputerChoose(){
-    const CpuChoice =['Paper', 'Rock', 'Scissors'];
-    let getIndex = Math.floor(Math.random()*CpuChoice.length);
-    let choiced = CpuChoice[getIndex]
-    
-    return choiced;
+function GenerateComputerChoose() {
+  const CpuChoice = ["paper", "rock", "scissors"];
+  let getIndex = Math.floor(Math.random() * CpuChoice.length);
+  let choiced = CpuChoice[getIndex];
+  return choiced;
 }
 
-/*
-function CreateChoiceOfCPU(choiced) {
-    if(choiced=="Paper") {
+function ComparePlayerToComputer(PlayerValue, ComputerValue) {
+  if (PlayerValue == ComputerValue) {
+    return console.log("Remis");
+  }
 
-        const newI = document.createElement('i');
-        newI.classList.add('icon-hand-paper-o');
-        const newDIV = document.createElement('div');
-        newDIV.classList.add('paper');
-        newDIV.appendChild(newI);
-
-    } else if(choiced=="Rock") {
-
-        const newI = document.createElement('i');
-        newI.classList.add("icon-hand-grab-o");
-        const newDIV = document.createElement('div');
-        newDIV.classList.add('rock');
-        newDIV.appendChild(newI);
-
-    } else if(choiced=="Scissors") {
-
-        const newI = document.createElement('i');
-        newI.classList.add("icon-hand-scissors-o");
-        const newDIV = document.createElement('div');
-        newDIV.classList.add('scissors');
-        newDIV.appendChild(newI);
-
-    }   
+  if (PlayerValue == "scissors" && ComputerValue == "rock") {
+    return console.log("Komputer wygrywa");
+  } else if (PlayerValue == "paper" && ComputerValue == "scissors") {
+    return console.log("Komputer wygrywa");
+  } else if (PlayerValue == "rock" && ComputerValue == "paper") {
+    return console.log("Komputer wygrywa");
+  } else {
+    return console.log("Wygrywasz");
+  }
 }
-
-*/
-
-})();
