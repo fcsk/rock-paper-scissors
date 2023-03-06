7340ac3d2c7339a40a9fddf15860d6cbeadd1e64
fcsk <kfocusek@gmail.com>
2023-03-06 15:06:08 +0100

    After fixing bugs, the computer's response is only displayed in one image. The next keystroke gives the computer a new response. Minor improvement with point reset.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 // @ts-nocheck
 const selectedDiV = document.querySelector(".player-weapon");
 const resetButton = document.querySelector(".button-reset");
-console.log(resetButton);
+const resultdiv = document.querySelector(".result");
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -15,15 +16,17 @@ selectedDiV?.addEventListener("click", function (event) {
   } else if (string == "icon-hand-grab-o" || string == "rock") {
     value = "rock";
   }
-  
+
   const playerValue = value;
-  const computerValue = GenerateComputerChoose();
+  let computerValue = GenerateComputerChoose();
   console.log("Grasz wybrał", playerValue);
   console.log("Komputer wybrał", computerValue);
-  setTimeout(GenerateComputerDIV(computerValue), 700);
-  RemoveComputerDiv()
+  setTimeout(function () {
+    GenerateComputerDIV(computerValue);
+  }, 200);
+
+  RemoveComputerDiv();
   ComparePlayerToComputer(playerValue, computerValue);
-  
 });
 
 function GenerateComputerChoose() {
@@ -34,127 +37,93 @@ function GenerateComputerChoose() {
 }
 
 function ComparePlayerToComputer(PlayerValue, ComputerValue) {
-  let player = 'p';
-  let computer = 'c';
-  let draw ='d';
-  
-  if (PlayerValue == ComputerValue) {
+  let player = "p";
+  let computer = "c";
+  let draw = "d";
 
-    document.getElementById("computer-choose").innerHTML = "It's a draw";
+  if (PlayerValue == ComputerValue) {
+    setTimeout(function () {
+      document.getElementById("computer-choose").innerHTML = "It's a draw";
+    }, 200);
+
     return CounterResult(draw);
   } else if (PlayerValue == "scissors" && ComputerValue == "rock") {
-
-    document.getElementById("computer-choose").innerHTML = "Computer won";
+    setTimeout(function () {
+      document.getElementById("computer-choose").innerHTML = "Computer won";
+    }, 200);
     return CounterResult(computer);
-
   } else if (PlayerValue == "paper" && ComputerValue == "scissors") {
-
-    document.getElementById("computer-choose").innerHTML = "Computer won";
+    setTimeout(function () {
+      document.getElementById("computer-choose").innerHTML = "Computer won";
+    }, 200);
     return CounterResult(computer);
-
   } else if (PlayerValue == "rock" && ComputerValue == "paper") {
-
-    document.getElementById("computer-choose").innerHTML = "Computer won";
+    setTimeout(function () {
+      document.getElementById("computer-choose").innerHTML = "Computer won";
+    }, 200);
     return computer;
   } else {
-
-    document.getElementById("computer-choose").innerHTML = "You won";
+    setTimeout(function () {
+      document.getElementById("computer-choose").innerHTML = "You won";
+    }, 200);
     return CounterResult(player);
   }
 }
 
 function GenerateComputerDIV(computerValue) {
-      const iElement = document.createElement('i');
-      const opponentDiv = document.createElement('div');
-      const resultdiv = document.querySelector('.result');
+  const iElement = document.createElement("i");
+  const opponentDiv = document.createElement("div");
 
-      if (computerValue == "scissors") {
-
-        iElement.classList.add("icon-hand-scissors-o");
-        opponentDiv.classList.add('opponent');
-        opponentDiv.append(iElement);
-        resultdiv.appendChild(opponentDiv);
-        
-
-
-      } else if (computerValue == "rock") {
-
-        iElement.classList.add("icon-hand-grab-o");
-        opponentDiv.classList.add('opponent');
-        opponentDiv.append(iElement);
-        resultdiv.appendChild(opponentDiv);
-        
-
-        
-      } else if (computerValue == "paper") {
-
-        iElement.classList.add("icon-hand-paper-o");
-        opponentDiv.classList.add('opponent');
-        opponentDiv.append(iElement);
-        resultdiv.appendChild(opponentDiv);
-       
-
-      }
+  if (computerValue == "scissors") {
+    iElement.classList.add("icon-hand-scissors-o");
+    opponentDiv.classList.add("opponent");
+    opponentDiv.append(iElement);
+    resultdiv.appendChild(opponentDiv);
+  } else if (computerValue == "rock") {
+    iElement.classList.add("icon-hand-grab-o");
+    opponentDiv.classList.add("opponent");
+    opponentDiv.append(iElement);
+    resultdiv.appendChild(opponentDiv);
+  } else if (computerValue == "paper") {
+    iElement.classList.add("icon-hand-paper-o");
+    opponentDiv.classList.add("opponent");
+    opponentDiv.append(iElement);
+    resultdiv.appendChild(opponentDiv);
+  }
 }
 
-function RemoveComputerDiv(){
-
-  setTimeout(function() {
-    document.querySelector(".opponent")?.remove();
-    document.getElementById("computer-choose").innerHTML = "";
-  }, 1300);
-        
+function RemoveComputerDiv() {
+  document.querySelector(".opponent")?.remove();
+  document.getElementById("computer-choose").innerHTML = "";
 }
 
 function CounterResult(result) {
-
-  if(result ==='p') {
+  if (result === "p") {
     playerScore++;
-    console.log("PlayerScore: ",playerScore);
-  } else if (result ==='c') {
+  } else if (result === "c") {
     computerScore++;
-    console.log("Komputer score: ",computerScore);
-  } else if ( result ==='d') {
-    console.log("remis");
+  } else if (result === "d") {
+    computerScore;
+    playerScore;
   } else {
-    console.log("Wystąpił błąd");
+    document.getElementById("computer-choose").innerHTML = "Wystąpił błąd";
   }
 
-  console.log( "Komputer score after: ", computerScore);
-  console.log( "Player score after: ", playerScore);
-  AddScoreToSite(computerScore,playerScore);
+  AddScoreToSite(computerScore, playerScore);
 }
 
 function AddScoreToSite(computerScore, playerScore) {
-  const  menu = document.querySelector('.menu-score');
-  menu.innerHTML ="";
+  const menu = document.querySelector(".menu-score");
+  menu.innerHTML = "";
   menu.innerHTML += `<li>Player: ${playerScore}</li><li>Computer: ${computerScore}</li>`;
 }
 
+resetButton.addEventListener("click", function () {
+  document.getElementById("computer-choose").innerHTML = "";
+  document.querySelector(".opponent")?.remove();
+  playerScore = 0;
+  computerScore = 0;
 
-
- resetButton.addEventListener("click", function () {
-   document.getElementById("computer-choose").innerHTML = "";
-   document.querySelector(".opponent")?.remove();
-   playerScore = 0;
-   computerScore = 0;
-
-   const  menu = document.querySelector('.menu-score');
-   menu.innerHTML = `<li>Player: 0</li><li>Computer: 0</li>`;
-
-
-
-   console.log(" po resecie computer: ", computerScore);
-   console.log("po resecie grasz: ", playerScore);
- });
-
-
-
-
-
- /* Zamiast 3 divow trzeba zrobic 3 przyciski zeby dobrze pobierało wartośći 
- 
-    i zamiast tworzyć div oponnent zrób diva ukrytego i tylko zmieniaj klasy/ dodadwaj je usuwaj 
- 
- 
- */
+  const menu = document.querySelector(".menu-score");
+  menu.innerHTML = `<li>Player: ${playerScore}</li><li>Computer: ${computerScore}</li>`;
+});
